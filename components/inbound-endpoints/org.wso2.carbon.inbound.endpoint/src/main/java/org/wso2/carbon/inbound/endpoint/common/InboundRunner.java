35cc8a2ae539698910ab19159922ce1ee736fe29
RaivU <ravi.undupitiya@gmail.com>
2016-03-26 21:51:43 +0530

    Refactored pinned server implementation. Removed manager node override and made implementation generic without breaking API by strictly implementing only for File Inbound.

--- components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/common/InboundRunner.java
+++ components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/common/InboundRunner.java
@@ -17,16 +17,18 @@
  */
 package org.wso2.carbon.inbound.endpoint.common;
 
-import java.util.Date;
-
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.core.multitenancy.utils.TenantAxisUtils;
 import org.wso2.carbon.inbound.endpoint.persistence.service.InboundEndpointPersistenceServiceDSComponent;
+import org.wso2.carbon.inbound.endpoint.protocol.file.FileTask;
 import org.wso2.carbon.mediation.clustering.ClusteringAgentUtil;
 import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.carbon.utils.ConfigurationContextService;
+
+import java.util.Date;
+import java.util.Properties;
 
 /**
  * 
@@ -46,7 +48,7 @@ public class InboundRunner implements Runnable {
     private long currentRuntime;
     private long cycleInterval;
     private String tenantDomain;
-    private boolean runOnManagerOverride;
+    private boolean runOnManagerOverride = false;
 
     private static final Log log = LogFactory.getLog(InboundRunner.class);
 
@@ -54,8 +56,18 @@ public class InboundRunner implements Runnable {
         this.task = task;
         this.interval = interval;
         this.tenantDomain = tenantDomain;
-        this.runOnManagerOverride = Boolean.parseBoolean(
-                System.getProperty("runOnManager", "false"));
+
+        // This is to add pinnedServer support to file task. This is in place ONLY for
+        // FileTask for now and not provided in generic manner in order not to make API changes.
+        Properties inboundProperties = null;
+        if (task instanceof FileTask) {
+            FileTask ft = (FileTask) task;
+            inboundProperties = ft.getInboundProperties();
+            // We only need to check if pinned server is enabled and not if this server is pinned
+            // server as this is done in InboundRequestProcessorImpl before arriving at this point.
+            this.runOnManagerOverride = InboundRequestProcessorImpl
+                    .isPinnedServerEnabled(inboundProperties);
+        }
     }
 
     /**
